b969e8e716badf835e0e331c62f578a9c3e6434f
whitehole07 <danieleagamennone@gmail.com>
2024-04-28 17:14:25 +0800

    added ddpg implementation

---
+++ processing/RL/DDPG/noise/ou_noise.py
@@ -0,0 +1,36 @@
+import numpy as np
+import random
+import copy
+
+
+class OU_Noise(object):
+    """Ornstein-Uhlenbeck process.
+    code from :
+    https://math.stackexchange.com/questions/1287634/implementing-ornstein-uhlenbeck-in-matlab
+    The OU_Noise class has four attributes
+
+        size: the size of the noise vector to be generated
+        mu: the mean of the noise, set to 0 by default
+        theta: the rate of mean reversion, controlling how quickly the noise returns to the mean
+        sigma: the volatility of the noise, controlling the magnitude of fluctuations
+    """
+
+    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.seed = random.seed(seed)
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        """Update internal state and return it as a noise sample.
+        This method uses the current state of the noise and generates the next sample
+        """
+        dx = self.theta * (self.mu - self.state) + self.sigma * np.array(
+            [np.random.normal() for _ in range(len(self.state))])
+        self.state += dx
+        return self.state
